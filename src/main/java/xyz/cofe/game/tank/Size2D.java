bc9c263b9bb3ed1c9ac10ad9ccb395f6a5c691a4
Kamnev Georgiy <nt.gocha@gmail.com>
2020-12-27 22:31:48 +0500

    size 2d

--- src/main/java/xyz/cofe/game/tank/Size2D.java
+++ src/main/java/xyz/cofe/game/tank/Size2D.java
@@ -1,8 +1,29 @@
 package xyz.cofe.game.tank;
 
+/**
+ * Возвращает размер объекта
+ */
 public interface Size2D {
+    /**
+     * Ширина объекта
+     * @return ширина объекта
+     */
     double width();
+
+    /**
+     * Высота объекта
+     * @return высота объекта
+     */
     double height();
+
+    /**
+     * Площадь объекта
+     * @return площать объекта, по умолчанию простое умножение width * height
+     */
+    default double area(){
+        return width() * height();
+    }
+
     public static Size2D of(double width, double height ){
         return new Size2D() {
             @Override
