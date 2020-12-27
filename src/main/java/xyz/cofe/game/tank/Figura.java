ced8a71d0a21f10e3a221b44a3b2ea9edc30a6dd
Kamnev Georgiy <nt.gocha@gmail.com>
2020-12-27 22:32:04 +0500

    fix changes

--- src/main/java/xyz/cofe/game/tank/Figura.java
+++ src/main/java/xyz/cofe/game/tank/Figura.java
@@ -1,5 +1,76 @@
 package xyz.cofe.game.tank;
 
-public class Figura {
+import java.awt.*;
 
+/**
+ * Отображение объекта
+ */
+public class Figura implements Drawing {
+    public Figura(SpriteLine spriteLine){
+        if( spriteLine==null )throw new IllegalArgumentException( "spriteLine==null" );
+        this.spriteLine = spriteLine;
+    }
+    public Figura(SpriteLine spriteLine, double left, double top){
+        if( spriteLine==null )throw new IllegalArgumentException( "spriteLine==null" );
+        this.spriteLine = spriteLine;
+        this.left = left;
+        this.top = top;
+    }
+    public Figura( Figura sample ){
+        if( sample==null )throw new IllegalArgumentException( "sample==null" );
+        this.spriteLine = sample.getSpriteLine().clone();
+        this.left = sample.getLeft();
+        this.top = sample.getTop();
+    }
+    public Figura clone(){
+        return new Figura(this);
+    }
+
+    //region left : double - Верхний левый угол
+    protected double left;
+    public double left(){ return left; }
+    public double getLeft(){
+        return left;
+    }
+    public void setLeft(double left){
+        this.left = left;
+    }
+    public Figura left(double left){
+        setLeft(left);
+        return this;
+    }
+    //endregion
+    //region top : double - Верхний левый угол
+    protected double top;
+    public double top(){ return top; }
+    public double getTop(){
+        return top;
+    }
+    public void setTop(double top){
+        this.top = top;
+    }
+    public Figura top(double top){
+        setTop(top);
+        return this;
+    }
+    //endregion
+    //region location(left,top)
+    public Figura location(double left, double top ){
+        setLeft(left);
+        setTop(top);
+        return this;
+    }
+    //endregion
+    //region spriteLine - Последовательность кадров
+    protected final SpriteLine spriteLine;
+    public SpriteLine getSpriteLine(){
+        return spriteLine;
+    }
+    //endregion
+
+    @Override
+    public void draw(Graphics2D gs){
+        if( gs==null )throw new IllegalArgumentException( "gs==null" );
+        spriteLine.draw(gs, getLeft(), getTop());
+    }
 }
--- src/main/java/xyz/cofe/game/tank/SpriteLine.java
+++ src/main/java/xyz/cofe/game/tank/SpriteLine.java
@@ -1,6 +1,7 @@
 package xyz.cofe.game.tank;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,19 +11,53 @@ import java.util.stream.Stream;
 /**
  * "Раскадровка" - последовательность спрайтов
  */
-public class SpriteLine implements Drawing {
+@SuppressWarnings("UnusedReturnValue")
+public class SpriteLine implements PositionalDrawing {
+    /**
+     * Конструктор по умолчанию
+     */
     public SpriteLine(){
     }
 
+    /**
+     * Конструктор
+     * @param sprite спрайт
+     */
     public SpriteLine(Sprite sprite){
         if( sprite==null )throw new IllegalArgumentException( "sprite==null" );
         sprites.add(sprite);
     }
 
-    public SpriteLine(Iterable<Sprite> sprite, double duration){
-        if( sprite==null )throw new IllegalArgumentException( "sprite==null" );
-        sprite.forEach( sprites::add );
+    /**
+     * Конструктор
+     * @param sprites спрайты
+     * @param duration частота смены кадров в секундах
+     */
+    public SpriteLine(Iterable<Sprite> sprites, double duration){
+        if( sprites==null )throw new IllegalArgumentException( "sprites==null" );
+        sprites.forEach( this.sprites::add );
         this.duration = duration;
+    }
+
+    /**
+     * Конструктор копирования
+     * @param samples образец для копироавния
+     */
+    public SpriteLine(SpriteLine samples){
+        if( samples==null )throw new IllegalArgumentException( "samples==null" );
+        sprites.addAll(samples.sprites);
+        duration = samples.getDuration();
+        started = samples.getStarted();
+        stopped = samples.getStopped();
+        loop = samples.isLoop();
+    }
+
+    /**
+     * Клонирование
+     * @return клон
+     */
+    public SpriteLine clone(){
+        return new SpriteLine(this);
     }
 
     public SpriteLine configure(Consumer<SpriteLine> conf){
@@ -31,47 +66,112 @@ public class SpriteLine implements Drawing {
         return this;
     }
 
-    //region sprites : List<Sprite>
+    //region sprites : List<Sprite> - набор спрайтов
     private final List<Sprite> sprites = new ArrayList<>();
+
+    /**
+     * Возвращает набор спрайтов
+     * @return набор спрайтов
+     */
     public List<Sprite> getSprites(){ return Collections.unmodifiableList(sprites); }
+
+    /**
+     * Указывает набор спрайтов
+     * @param sprites набор спрайтов
+     */
     public void setSprites(List<Sprite> sprites){
+        if( sprites==null )throw new IllegalArgumentException( "sprites==null" );
         this.sprites.clear();
-        if( sprites!=null ){
-            this.sprites.addAll(sprites);
-        }
+        this.sprites.addAll(sprites);
     }
-    public void setSprites(Iterable<Sprite> sprites){
+
+    /**
+     * Указывает набор спрайтов
+     * @param sprites набор спрайтов
+     * @return Self ссылка
+     */
+    public SpriteLine sprites(Iterable<Sprite> sprites){
+        if( sprites==null )throw new IllegalArgumentException( "sprites==null" );
         this.sprites.clear();
-        if( sprites!=null ){
-            sprites.forEach(this.sprites::add);
-        }
+        sprites.forEach(this.sprites::add);
+        return this;
     }
-    public void setSprites(Stream<Sprite> sprites){
+
+    /**
+     * Указывает набор спрайтов
+     * @param sprites набор спрайтов
+     * @return Self ссылка
+     */
+    public SpriteLine sprites(Stream<Sprite> sprites){
+        if( sprites==null )throw new IllegalArgumentException( "sprites==null" );
         this.sprites.clear();
-        if( sprites!=null ){
-            sprites.forEach(this.sprites::add);
-        }
+        sprites.forEach(this.sprites::add);
+        return this;
+    }
+
+    /**
+     * Указывает набор спрайтов
+     * @param images набор спрайтов
+     * @return Self ссылка
+     */
+    public SpriteLine images(Iterable<BufferedImage> images){
+        if( images==null )throw new IllegalArgumentException( "images==null" );
+        sprites.clear();
+        images.forEach( im -> {
+            sprites.add(new Sprite(im));
+        });
+        return this;
     }
     //endregion
-    //region duration : double
+    //region duration : double - продолжительность смены кадров
     private double duration;
+
+    /**
+     * Возвращает продолжительность смены кадров в долях секунды
+     * @return продолжительность смены кадров
+     */
     public double getDuration(){ return duration; }
+
+    /**
+     * Указывает продолжительность смены кадров в долях секунды
+     * @param duration продолжительность смены кадров
+     */
     public void setDuration(double duration){ this.duration = duration; }
+
+    /**
+     * Указывает продолжительность смены кадров в долях секунды
+     * @param duration продолжительность смены кадров
+     * @return Self ссылка
+     */
+    public SpriteLine duration(double duration){
+        setDuration(duration);
+        return this;
+    }
     //endregion
-    //region started : long
+    //region started : long - Время начала анимации в System.nanoTime()
     private long started = 0;
     public long getStarted(){ return started; }
     protected void setStarted(long started){ this.started = started; }
     //endregion
-    //region stopped : long
+    //region stopped : long - Время завершения анимации в System.nanoTime()
     private long stopped = 0;
     public long getStopped(){ return stopped; }
     protected void setStopped(long stopped){ this.stopped = stopped; }
     //endregion
-    //region loop : boolean
+    //region loop : boolean - циклическая анимация
     private boolean loop = true;
     public boolean isLoop(){ return loop; }
     public void setLoop(boolean value){ this.loop = value; }
+
+    /**
+     * Указывает циклическая анимация или нет
+     * @param value true - циклическая
+     * @return Self ссылка
+     */
+    public SpriteLine loop(boolean value){
+        setLoop(value);
+        return this;
+    }
     //endregion
 
     /**
@@ -85,31 +185,32 @@ public class SpriteLine implements Drawing {
 
     /**
      * Запуск анимации
+     * @return Self ссылка
      */
-    public void start(){
+    public SpriteLine start(){
         stopped = 0;
         started = System.nanoTime();
+        return this;
     }
 
     /**
      * Остановка анимации
+     * @return Self ссылка
      */
-    public void stop(){
+    public SpriteLine stop(){
         stopped = System.nanoTime();
+        return this;
     }
 
-    @Override
-    public void draw(Graphics2D gs, int x, int y){
-        if( gs==null )throw new IllegalArgumentException( "gs==null" );
-
+    /**
+     * Возвращает индекс текущего кадра
+     * @return индекс текущего кадра
+     */
+    public int frame(){
         int spriteCount = sprites.size();
-        if( spriteCount<1 )return;
+        if( spriteCount<1 )return -1;
         if( spriteCount==1 || started==0 ){
-            Sprite s = sprites.get(0);
-            if( s==null )return;
-
-            s.draw(gs,x,y);
-            return;
+            return 0;
         }
 
         long tnow = System.nanoTime();
@@ -126,14 +227,22 @@ public class SpriteLine implements Drawing {
             frame = loop ? frame % spriteCount : spriteCount-1;
         }
 
+        return frame;
+    }
+
+    @Override
+    public void draw(Graphics2D gs, double x, double y){
+        if( gs==null )throw new IllegalArgumentException( "gs==null" );
+
+        int spriteCount = sprites.size();
+        int frame = frame();
+
         if( !loop && frame==(spriteCount-1) ){
             stopped = System.nanoTime();
         }
 
         Sprite s = sprites.get(frame);
         if( s==null )return;
-
-        //System.out.println("frameDuration="+frameDuration+" frame="+frame);
 
         s.draw(gs,x,y);
     }
