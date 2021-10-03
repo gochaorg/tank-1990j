b4641fbb5dfec5a5db8dcdecce18c202f9669224
Kamnev Georgiy <nt.gocha@gmail.com>
2021-10-04 03:26:33 +0500

    prepare scene

--- src/main/java/xyz/cofe/game/tank/ui/cmd/PlaySceneCommand.java
+++ src/main/java/xyz/cofe/game/tank/ui/cmd/PlaySceneCommand.java
@@ -2,6 +2,7 @@ package xyz.cofe.game.tank.ui.cmd;
 
 import xyz.cofe.game.tank.ui.MainFrame;
 import xyz.cofe.game.tank.unt.Figure;
+import xyz.cofe.game.tank.unt.LevelBrick;
 import xyz.cofe.game.tank.unt.Scene;
 
 import java.util.ArrayList;
@@ -38,7 +39,7 @@ public class PlaySceneCommand implements Runnable {
         mf.setSize(800,600);
         mf.setLocationRelativeTo(null);
 
-        mf.setScene(new Scene(scene,
+        var list =
             new ArrayList<Figure<?>>(
                 scene.getFigures().stream()
                     .map( f -> {
@@ -47,8 +48,56 @@ public class PlaySceneCommand implements Runnable {
                         return f;
                     } )
                     .collect(Collectors.toList())
-            )
-        ));
+            );
+
+        mf.setScene(new Scene(scene,list));
+
+        var levelBricks =
+            list.stream()
+                .filter( f -> f instanceof LevelBrick )
+                .map( f -> (LevelBrick<?>)f )
+                    .collect(Collectors.toList());
+
+        var lst = new ArrayList<LevelBrick<?>>();
+        for( LevelBrick<?> brick : levelBricks ){
+            lst.clear();
+            if( brick.isUpLeft() ){
+                var b = brick.clone();
+                b.state(LevelBrick.UL_BRICK);
+                lst.add(b);
+            }
+            if( brick.isUpRight() ){
+                var b = brick.clone();
+                b.state(LevelBrick.UL_BRICK);
+                b.location(
+                    brick.left()+brick.sprite().image().getWidth(),
+                    brick.top());
+                lst.add(b);
+            }
+            if( brick.isBottomLeft() ){
+                var b = brick.clone();
+                b.state(LevelBrick.UL_BRICK);
+                b.location(
+                    brick.left(),
+                    brick.top()+brick.sprite().image().getHeight());
+                lst.add(b);
+            }
+            if( brick.isBottomRight() ){
+                var b = brick.clone();
+                b.state(LevelBrick.UL_BRICK);
+                b.location(
+                    brick.left()+brick.sprite().image().getWidth(),
+                    brick.top()+brick.sprite().image().getHeight());
+                lst.add(b);
+            }
+            if( !lst.isEmpty() ){
+                var idx = list.indexOf(brick);
+                if( idx>=0 ){
+                    list.remove(idx);
+                    list.addAll(idx,lst);
+                }
+            }
+        }
 
         mf.setVisible(true);
         mf.toFront();
