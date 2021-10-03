d6c6a6fefe93b6477a11263cb66e0c1cdde71274
Kamnev Georgiy <nt.gocha@gmail.com>
2021-10-04 03:26:15 +0500

    play two - sprites

--- src/main/java/xyz/cofe/game/tank/unt/PlayerTwo.java
+++ src/main/java/xyz/cofe/game/tank/unt/PlayerTwo.java
@@ -19,31 +19,31 @@ public class PlayerTwo extends Player<PlayerTwo> {
         Map.of(
             PlayerState.Level0,
             Map.of(
-                Direction.LEFT, SpritesData.player_one_left_0.toSpriteLine(),
-                Direction.RIGHT, SpritesData.player_one_right_0.toSpriteLine(),
-                Direction.UP, SpritesData.player_one_up_0.toSpriteLine(),
-                Direction.DOWN, SpritesData.player_one_down_0.toSpriteLine()
+                Direction.LEFT, SpritesData.player_two_left_0.toSpriteLine(),
+                Direction.RIGHT, SpritesData.player_two_right_0.toSpriteLine(),
+                Direction.UP, SpritesData.player_two_up_0.toSpriteLine(),
+                Direction.DOWN, SpritesData.player_two_down_0.toSpriteLine()
             ),
             PlayerState.Level1,
             Map.of(
-                Direction.LEFT, SpritesData.player_one_left_1.toSpriteLine(),
-                Direction.RIGHT, SpritesData.player_one_right_1.toSpriteLine(),
-                Direction.UP, SpritesData.player_one_up_1.toSpriteLine(),
-                Direction.DOWN, SpritesData.player_one_down_1.toSpriteLine()
+                Direction.LEFT, SpritesData.player_two_left_1.toSpriteLine(),
+                Direction.RIGHT, SpritesData.player_two_right_1.toSpriteLine(),
+                Direction.UP, SpritesData.player_two_up_1.toSpriteLine(),
+                Direction.DOWN, SpritesData.player_two_down_1.toSpriteLine()
             ),
             PlayerState.Level2,
             Map.of(
-                Direction.LEFT, SpritesData.player_one_left_2.toSpriteLine(),
-                Direction.RIGHT, SpritesData.player_one_right_2.toSpriteLine(),
-                Direction.UP, SpritesData.player_one_up_2.toSpriteLine(),
-                Direction.DOWN, SpritesData.player_one_down_2.toSpriteLine()
+                Direction.LEFT, SpritesData.player_two_left_2.toSpriteLine(),
+                Direction.RIGHT, SpritesData.player_two_right_2.toSpriteLine(),
+                Direction.UP, SpritesData.player_two_up_2.toSpriteLine(),
+                Direction.DOWN, SpritesData.player_two_down_2.toSpriteLine()
             ),
             PlayerState.Level3,
             Map.of(
-                Direction.LEFT, SpritesData.player_one_left_3.toSpriteLine(),
-                Direction.RIGHT, SpritesData.player_one_right_3.toSpriteLine(),
-                Direction.UP, SpritesData.player_one_up_3.toSpriteLine(),
-                Direction.DOWN, SpritesData.player_one_down_3.toSpriteLine()
+                Direction.LEFT, SpritesData.player_two_left_3.toSpriteLine(),
+                Direction.RIGHT, SpritesData.player_two_right_3.toSpriteLine(),
+                Direction.UP, SpritesData.player_two_up_3.toSpriteLine(),
+                Direction.DOWN, SpritesData.player_two_down_3.toSpriteLine()
             )
         );
 
